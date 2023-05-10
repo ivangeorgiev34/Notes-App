356c0d0f291410b8c30b4d19ca7269e2b33b15b8
ivangeorgiev34 <ivan.georgiev0904@gmail.com>
2023-05-10 16:12:07 +0300

    Add database models(Note and User)

---
+++ NotesApp.Infrastructure/Models/Note.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NotesApp.Infrastructure.Models
+{
+    public class Note
+    {
+        [Required]
+        public Guid Id { get; set; }
+
+        [Required]
+        public string Title { get; set; } = null!;
+
+        [Required]
+        public string Description { get; set; } = null!;
+
+        public bool IsActive { get; set; } = true;
+
+        public User User { get; set; } = null!;
+    }
+}
