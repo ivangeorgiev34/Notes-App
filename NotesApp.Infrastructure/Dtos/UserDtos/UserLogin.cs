498828dbf1ae486a9addd71069e6f276c520d461
ivangeorgiev34 <ivan.georgiev0904@gmail.com>
2023-05-10 16:09:44 +0300

    Add UserDtos

---
+++ NotesApp.Infrastructure/Dtos/UserDtos/UserLogin.cs
@@ -0,0 +1,23 @@
+﻿using NotesApp.Infrastructure.Constants;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NotesApp.Infrastructure.Dtos.UserDtos
+{
+    public class UserLogin
+    {
+
+        [Required]
+        [EmailAddress(ErrorMessage = "Invalid email address!")]
+        public string Email { get; set; } = null!;
+
+        [Required]
+        [StringLength(InfrastructureConstants.User.USER_PASSWORD_MAX_LENGTH, MinimumLength = InfrastructureConstants.User.USER_USERNAME_MIN_LENGTH, ErrorMessage = "Password must be between 6 and 12 symbols!")]
+        public string Password { get; set; } = null!;
+
+    }
+}
---
+++ NotesApp.Infrastructure/Dtos/UserDtos/UserRegister.cs
@@ -0,0 +1,26 @@
+﻿using NotesApp.Infrastructure.Constants;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NotesApp.Infrastructure.Dtos.UserDtos
+{
+    public class UserRegister
+    {
+        [Required]
+        [EmailAddress(ErrorMessage = "Invalid email address!")]
+        public string Email { get; set; } = null!;
+
+        [Required]
+        [StringLength(InfrastructureConstants.User.USER_USERNAME_MAX_LENGTH, MinimumLength = InfrastructureConstants.User.USER_USERNAME_MIN_LENGTH, ErrorMessage = "Username must be between 6 and 12 symbols!")]
+        public string Username { get; set; } = null!;
+
+        [Required]
+        [StringLength(InfrastructureConstants.User.USER_PASSWORD_MAX_LENGTH, MinimumLength = InfrastructureConstants.User.USER_USERNAME_MIN_LENGTH, ErrorMessage = "Password must be between 6 and 12 symbols!")]
+        public string Password { get; set; } = null!;
+
+    }
+}
