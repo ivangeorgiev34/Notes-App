ac117f2eda89e0a481792afd516010583c796ae1
ivangeorgiev34 <ivan.georgiev0904@gmail.com>
2023-05-10 16:11:05 +0300

    Add InfrastructureConstants class for needed constants through out the Infrasturcture project

---
+++ NotesApp.Infrastructure/Constants/InfrastructureConstants.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NotesApp.Infrastructure.Constants
+{
+    public static class InfrastructureConstants
+    {
+        public static class User
+        {
+            public const int USER_USERNAME_MIN_LENGTH = 6;
+            public const int USER_USERNAME_MAX_LENGTH = 12;
+
+            public const int USER_PASSWORD_MIN_LENGTH = 6;
+            public const int USER_PASSWORD_MAX_LENGTH = 12;
+        }
+    }
+}
