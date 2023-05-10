29dd13d06e9e19d87624f2b1f12f917c4e497e2d
ivangeorgiev34 <ivan.georgiev0904@gmail.com>
2023-05-10 16:11:31 +0300

    Add NotesDbContext

---
+++ NotesApp.Infrastructure/Data/NotesDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NotesApp.Infrastructure.Models;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace NotesApp.Infrastructure.Data
+{
+    public class NotesDbContext : DbContext
+    {
+        public NotesDbContext(DbContextOptions<NotesDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<User>? Users { get; set; }
+
+        public DbSet<Note>? Notes { get; set; }
+
+    }
+}
