6a64bd4d2c5a6a670543909d4d0291f896248998
ivangeorgiev34 <ivan.georgiev0904@gmail.com>
2023-05-10 16:04:48 +0300

    Add service collection extension

---
+++ NotesApp/Extensions/NotesAppServiceCollectionExtention.cs
@@ -0,0 +1,33 @@
+﻿using NotesApp.Core.Contracts;
+using NotesApp.Core.Services;
+using NotesApp.Infrastructure.Common;
+
+namespace NotesApp.Extensions
+{
+    public static class NotesAppServiceCollectionExtention
+    {
+        /// <summary>
+		/// Adds all the services to the IoC of the application
+		/// </summary>
+		/// <param name="services"></param>
+		/// <returns></returns>
+        /// 
+
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+        {
+            services.AddScoped<IRepository, Repository>();
+            services.AddScoped<IUserService, UserService>();
+            //services.AddScoped<IStudentService, StudentService>();
+            //services.AddScoped<IClassService, ClassService>();
+            //services.AddScoped<ISubjectService, SubjectService>();
+            //services.AddScoped<ITeacherService, TeacherService>();
+            //services.AddScoped<IProfileService, ProfileService>();
+            //services.AddScoped<IGradeService, GradeService>();
+            //services.AddScoped<IRemarkService, RemarkService>();
+            //services.AddScoped<IComplimentService, ComplimentService>();
+
+            return services;
+        }
+
+    }
+}
