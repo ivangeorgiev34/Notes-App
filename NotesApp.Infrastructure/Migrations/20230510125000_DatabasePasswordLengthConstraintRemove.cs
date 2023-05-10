4c0b08e2c3e0f467d76f207bd4d8ac571df46ad1
ivangeorgiev34 <ivan.georgiev0904@gmail.com>
2023-05-10 16:15:54 +0300

    Add RemovePasswordLengthConstraint migration

---
+++ NotesApp.Infrastructure/Migrations/20230510125000_DatabasePasswordLengthConstraintRemove.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace NotesApp.Infrastructure.Migrations
+{
+    public partial class DatabasePasswordLengthConstraintRemove : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Password",
+                table: "Users",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(12)",
+                oldMaxLength: 12);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Password",
+                table: "Users",
+                type: "nvarchar(12)",
+                maxLength: 12,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+    }
+}
