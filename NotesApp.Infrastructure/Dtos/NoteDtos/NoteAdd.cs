9dc22fde62f0558afd40b3ca58b548c6ce0eb3e0
ivangeorgiev34 <ivan.georgiev0904@gmail.com>
2023-05-12 18:01:15 +0300

    Add NoteAdd DTO initial commit

---
+++ NotesApp.Infrastructure/Dtos/NoteDtos/NoteAdd.cs
@@ -0,0 +1,22 @@
+﻿using NotesApp.Infrastructure.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NotesApp.Infrastructure.Dtos.NoteDtos
+{
+    public class NoteAdd
+    {
+
+        [Required]
+        public string Title { get; set; } = null!;
+
+        [Required]
+        public string Description { get; set; } = null!;
+
+
+    }
+}
