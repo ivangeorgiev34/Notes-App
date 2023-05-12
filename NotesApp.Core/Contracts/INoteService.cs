8bf118e2ba5917d10952027a0e166fa371438df9
ivangeorgiev34 <ivan.georgiev0904@gmail.com>
2023-05-12 17:55:00 +0300

    Add INoteService interface

---
+++ NotesApp.Core/Contracts/INoteService.cs
@@ -0,0 +1,23 @@
+﻿using NotesApp.Infrastructure.Dtos.NoteDtos;
+using NotesApp.Infrastructure.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NotesApp.Core.Contracts
+{
+    public interface INoteService
+    {
+        Task AddNoteAsync(string userId, NoteAdd noteAdd);
+
+        Task<List<Note>?> GetAllUserNotesAsync(string userId);
+
+        Task<Note?> GetNoteByIdAndUserIdAsync(string userId,string noteId);
+
+        Task<bool> DeleteNoteByIdAndUserIdAsync(string userId, string noteId);
+
+        Task<bool> EditNoteByIdAndUserIdAsync(string userId, string noteId,NoteUpdate noteUpdate);
+    }
+}
